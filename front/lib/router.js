d1842e081426356d907c9b14d178be396b1a50a5
matleppa <matti.leppanen@apinf.io>
2017-01-19 11:43:24 +0200

    Added comments

--- front/lib/router.js
+++ front/lib/router.js
@@ -1,9 +1,15 @@
+/*
+Front paage
+*/
 FlowRouter.route('/', {
     action: function(params, queryParams) {
         BlazeLayout.render('mainLayout', {content: 'postsList'})
     }
 });
 
+/*
+Insert new post
+*/
 FlowRouter.route('/new', {
     action: function(params, queryParams) {
         BlazeLayout.render('mainLayout', {content: 'insertPage'})
