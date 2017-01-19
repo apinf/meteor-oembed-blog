e0603794eeb981a51a7527ecef373fa3dba5b47d
matleppa <matti.leppanen@apinf.io>
2017-01-19 10:36:08 +0200

    Selection New is linked to insertPage instead of  postsForm.

--- front/lib/router.js
+++ front/lib/router.js
@@ -6,6 +6,6 @@ FlowRouter.route('/', {
 
 FlowRouter.route('/new', {
     action: function(params, queryParams) {
-        BlazeLayout.render('mainLayout', {content: 'postsForm'})
+        BlazeLayout.render('mainLayout', {content: 'insertPage'})
     }
 });
