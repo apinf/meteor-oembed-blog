d203744a1d9642229624563cabfb8e94fe6f1819
jawidahmadi <jawid.ahmadi@apinf.io>
2017-01-23 14:30:05 +0200

    Autoform addHooks has been added to close the Modal after AddPost

--- posts/client/form/form.js
+++ posts/client/form/form.js
@@ -9,3 +9,11 @@ Template.postsForm.helpers({
 Template.registerHelper('formatDate', function(date) {
   return moment(date).format('YYYY-MM-DD');
 });
+
+
+  AutoForm.addHooks(['postsForm'],{
+    onSuccess: function(operation, result, template){
+      // console.log("Autoclosemodal");
+      $('#postInsertModal').modal('hide');
+    }
+  })
