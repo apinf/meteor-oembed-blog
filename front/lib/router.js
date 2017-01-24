58109844afc32252d47e5dadc21353cc4c8e40c8
matleppa <matti.leppanen@apinf.io>
2017-01-24 15:19:27 +0200

    Added parameter to postsList call to indicate if it shows all posts or just user's own posts

--- front/lib/router.js
+++ front/lib/router.js
@@ -1,9 +1,16 @@
+function checkLoggedIn (ctx, redirect) {
+  if (!Meteor.userId()) {
+    redirect('/')
+  }
+}
+
+
 /*
 Front paage
 */
 FlowRouter.route('/', {
     action: function(params, queryParams) {
-        BlazeLayout.render('mainLayout', {content: 'postsList'})
+        BlazeLayout.render('mainLayout', {content: 'postsList', postTitle: 'all'})
     }
 });
 
@@ -13,5 +20,14 @@ Insert new post
 FlowRouter.route('/new', {
     action: function(params, queryParams) {
         BlazeLayout.render('mainLayout', {content: 'insertPage'})
+    }
+});
+
+/*
+List user's own posts
+*/
+FlowRouter.route('/myposts', {
+    action: function(params, queryParams) {
+        BlazeLayout.render('mainLayout', {content: 'postsList', postTitle: 'my'})
     }
 });
