d92a048a673687e4d8c923b84152afefff49d409
matleppa <matti.leppanen@apinf.io>
2017-01-25 09:26:34 +0200

    Separated own helpers for postlists

--- posts/client/list/posts.js
+++ posts/client/list/posts.js
@@ -1,13 +1,13 @@
 import { Posts } from '/posts/collection';
 
 Template.postsList.helpers({
-  posts (who) {
-    // console.log("who = ", who);
-    if (who === 'my') {
-      return Posts.find({ userId: Meteor.userId() }, {sort: {createdAt: -1}}).fetch();
-    }
-    else {
-      return Posts.find({}, {sort: {createdAt: -1}}).fetch();
-    }
+  posts () {
+    return Posts.find({}, {sort: {createdAt: -1}}).fetch();
+  }
+});
+
+Template.myPostsList.helpers({
+  posts () {
+    return Posts.find({ userId: Meteor.userId() }, {sort: {createdAt: -1}}).fetch();
   }
 });
