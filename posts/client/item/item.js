1032680e976afb11198cb452313783f9f71f497f
matleppa <matti.leppanen@apinf.io>
2017-01-25 09:25:06 +0200

    Moved button helpers from all posts to owner's posts

--- posts/client/item/item.js
+++ posts/client/item/item.js
@@ -14,7 +14,22 @@ Template.postItem.onRendered(function () {
   postElement.oembed();
 });
 
-Template.postItem.helpers({
+Template.myPostItem.onRendered(function () {
+  // Get reference to template instance
+  const instance = this;
+
+  // Get Post ID from template instance
+  const postId = instance.data.post._id;
+
+  // Get reference to post DOM element
+  const postElement = instance.$(`#${postId}`);
+
+  // Render OEmbed content
+  postElement.oembed();
+});
+
+
+Template.myPostItem.helpers({
   userIsPostOwner: function () {
     const instance = Template.instance();
 
@@ -25,7 +40,7 @@ Template.postItem.helpers({
   },
 })
 
-Template.postItem.events({
+Template.myPostItem.events({
   'click .delete'() {
     if (confirm("Do you really want delete this post?")) {
       const instance = Template.instance();
