8e5b6b72a8246322518bd1ad235b544fc2ea0bdc
jawidahmadi <jawid.ahmadi@apinf.io>
2017-01-27 15:34:03 +0200

    Added subscription or posts

--- posts/client/my_posts/myPosts.js
+++ posts/client/my_posts/myPosts.js
@@ -1,5 +1,9 @@
 import { Posts } from '/posts/collection';
 
+Template.myPostsList.onCreated(function bodyOnCreated() {
+  this.subscribe('posts');
+});
+
 Template.myPostsList.helpers({
   posts () {
     return Posts.find({ userId: Meteor.userId() }, {sort: {createdAt: -1}}).fetch();
