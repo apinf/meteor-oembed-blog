07e5352625da21aa331316264dd2fc9764f8ffb6
jawidahmadi <jawid.ahmadi@apinf.io>
2017-01-27 15:32:34 +0200

    Added publishing

--- posts/collection/index.js
+++ posts/collection/index.js
@@ -3,3 +3,10 @@ import { Mongo } from 'meteor/mongo';
 const Posts = new Mongo.Collection('posts');
 
 export { Posts };
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('posts', function postsPublication() {
+    return Posts.find();
+  });
+}
