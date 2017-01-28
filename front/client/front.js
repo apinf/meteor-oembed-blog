7569cf96a519b8809e372f2764828a36eb030ef8
Matti Leppänen <mainos.lep@gmail.com>
2017-01-28 18:26:26 +0200

    Added subscription of collection 'posts'

--- front/client/front.js
+++ front/client/front.js
@@ -1,5 +1,9 @@
 import { Posts } from '/posts/collection';
 
+Template.frontPage.onCreated(function bodyOnCreated() {
+  this.subscribe('posts');
+});
+
 Template.frontPage.helpers({
   posts () {
     return Posts.find({}, {sort: {createdAt: -1}}).fetch();
