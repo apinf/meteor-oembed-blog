3b2d9d6dacf4f964bb409a1d31564a58278f6956
jawidahmadi <jawid.ahmadi@apinf.io>
2017-01-30 16:11:02 +0200

    applied pagination in front.js

--- front/client/front.js
+++ front/client/front.js
@@ -1,11 +1,32 @@
+// import { Posts } from '/posts/collection';
+//
+// Template.frontPage.onCreated(function bodyOnCreated() {
+//   this.subscribe('posts');
+// });
+//
+
+
+
 import { Posts } from '/posts/collection';
 
-Template.frontPage.onCreated(function bodyOnCreated() {
-  this.subscribe('posts');
-});
+Template.frontPage.onCreated(function() {
 
+ const instance = this;
+  // Set initial settings of pagination
+     instance.pagination = new Meteor.Pagination(Posts, {
+      // Count of cards in catalog
+      perPage: 4,
+      // Set sort by name on default
+      sort: { createdAt: -1 },
+})
+});
 Template.frontPage.helpers({
   posts () {
-    return Posts.find({}, {sort: {createdAt: -1}}).fetch();
-  }
+    // Return items of organization collection via Pagination
+    return Template.instance().pagination.getPage();
+  },
+  templatePagination () {
+    // Get reference of pagination
+    return Template.instance().pagination;
+  },
 });
