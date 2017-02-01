b920faa17829a0a582cac7ec58aedae31af2e997
matleppa <matti.leppanen@apinf.io>
2017-02-01 08:00:26 +0200

    Corrected indentations

--- posts/collection/index.js
+++ posts/collection/index.js
@@ -1,24 +1,9 @@
-// import { Mongo } from 'meteor/mongo';
-// const Posts = new Mongo.Collection('posts');
-// new Meteor.Pagination(Posts);
-// export { Posts };
-//
-// // if (Meteor.isServer) {
-// //   // This code only runs on the server
-// //   Meteor.publish('posts', function postsPublication() {
-// //     return Posts.find();
-// //   });
-// // }
-// if (Meteor.isServer) {
-//   // This code only runs on the server
-// new Meteor.Pagination(Posts);
-// }
-// index.js file here.....
 import { Mongo } from 'meteor/mongo';
 const Posts = new Mongo.Collection('posts');
 export { Posts };
 
+// This code only runs on the server
 if (Meteor.isServer) {
-  // This code only runs on the server
-new Meteor.Pagination(Posts);
+  // Use pagination when posts are requested
+  new Meteor.Pagination(Posts);
 }
